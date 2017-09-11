6420900fd785789ba4f41ee61781e2e380c5ef61
Dykam <dev@dykam.nl>
2017-09-11 15:16:23 +0200

    Made TestRunner usable as a (.Net) assembly runner

--- TestRunner/Program.cs
+++ TestRunner/Program.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
+using NDesk.Options;
+using NDesk.Options.Extensions;
 
 namespace TestRunner
 {
@@ -12,33 +15,74 @@ namespace TestRunner
     {
         static void Main(string[] args)
         {
-            // Repeat runs for more accurate timings
-            var warmupRuns = 1;
-            var testRuns = 1;
+            var os = new OptionSet();
 
-            // 1. Refer each project containing test programs and reference the main method by key (optional)
-            var tests = new Dictionary<string, Action>()
+            var entryPointVar= os.AddVariable<string>("entrypoint", "Custom entrypoint as fully qualified type name");
+            var entryTypeVar = os.AddVariable<string>("entryclass", "Custom entrypoint class as fully qualified type name");
+            var testFolderVar = os.AddVariable<string>("testFolder", "Folder containing testcases");
+            var inputFolderVar = os.AddVariable<string>("inputFolder", "Subfolder of test folder for input files");
+            var outputFolderVar = os.AddVariable<string>("outputFolder", "Subfolder of test folder for output files");
+            var inputFileMatchVar = os.AddVariable<string>("inputFileMatch", "Expression used when looking for input test files");
+            var outputFileMatchVar = os.AddVariable<string>("outputFileMatch", "Expression used when looking for output test files");
+            var warmupRunsVar = os.AddVariable<int>("warmupRuns");
+            var testRunsVar = os.AddVariable<int>("testRuns");
+
+            var exe = string.Join(" ", os.Parse(args)).Trim();
+
+            if (string.IsNullOrWhiteSpace(exe))
             {
-                ["ATest"] = ATest.Program.Main
+                os.WriteOptionDescriptions(Console.Out);
+                return;
+            }
+
+            var module = Assembly.LoadFrom(exe);
+
+            var testFolder = testFolderVar.Value ?? Path.GetDirectoryName(exe);
+            if (string.IsNullOrWhiteSpace(testFolder)) testFolder = Environment.CurrentDirectory;
+            var inputFolder = Path.GetFullPath(inputFolderVar.Value != null ? Path.Combine(testFolder, inputFolderVar) : testFolder);
+            var outputFolder = Path.GetFullPath(outputFolderVar.Value != null ? Path.Combine(testFolder, outputFolderVar) : testFolder);
+            var inputFileMatch = inputFileMatchVar.Value ?? "input";
+            var outputFileMatch = outputFileMatchVar.Value ?? "output";
+
+            (Type returnType, Type[] parameterTypes, Func<MethodInfo, Action> adaptor)[] entryAdaptors = {
+                (typeof(void), new Type[0], mi => (Action)mi.CreateDelegate(typeof(Action))),
+                (typeof(void), new[] {typeof(string).MakeArrayType()}, mi => () => ((Action<string[]>) mi.CreateDelegate(typeof(Action<string[]>)))(Array.Empty<string>())),
+                (typeof(int), new Type[0], mi => () => ((Func<int>)mi.CreateDelegate(typeof(Func<int>)))()),
+                (typeof(int), new[] {typeof(string).MakeArrayType()}, mi => () => ((Func<string[], int>) mi.CreateDelegate(typeof(Func<string[], int>)))(Array.Empty<string>())),
             };
 
-            // 2. Set test name and pick the main method to run. In case you hardcode the main method you can skip the previous step
-            var testName = args[0];
-            var testMethod = tests[testName];
+            Action entrypoint;
+            if (entryPointVar.Value != null || entryTypeVar.Value != null)
+            {
+                var entryType = entryTypeVar == null ? module.EntryPoint.DeclaringType : module.GetType(entryTypeVar);
+                var entryMethodName = entryPointVar.Value ?? "Main";
+                var methods = from method in entryType.GetMethods(BindingFlags.Static)
+                    where method.Name == entryMethodName
+                    select BindMethodEntryPoint(method);
 
-            // 3. Set the input/output file extensions
-            var inExtension = "in";
-            var outExtension = "out";
+                entrypoint = methods.FirstOrDefault();
+                if (entrypoint == null)
+                {
+                    Console.Error.WriteLine("Entrypoint not found, invalid entry point defined.");
+                    os.WriteOptionDescriptions(Console.Out);
+                    return;
+                }
+            }
 
-            // 4. Ensure testfiles are in place. By default looks for %WORKINGDIRECTORY%/${testName}/*.${inExtension}
-            // 5. Run TestRunner
+            entrypoint = BindMethodEntryPoint(module.EntryPoint);
+
+            // Repeat runs for more accurate timings
+            var testRuns = Math.Max(testRunsVar.Value, 1);
+            var warmupRuns = warmupRunsVar.Value == 0 ? testRuns : warmupRunsVar.Value;
+
             var stopwatch = new Stopwatch();
             var totalElapsed = TimeSpan.Zero;
             var passed = 0;
             var failed = 0;
-            foreach (var fileNameIn in Directory.GetFiles(testName, $"*.{inExtension}"))
+            foreach (var fileNameIn in Directory.GetFiles(Path.GetFullPath(inputFolder), $"*{inputFileMatch}*"))
             {
-                var fileNameOut = Path.ChangeExtension(fileNameIn, outExtension);
+                var fileNameOut = Path.Combine(outputFolder, Path.GetFileName(fileNameIn).Replace(inputFileMatch, outputFileMatch));
+                if (!File.Exists(fileNameOut)) continue;
 
                 string[] outputLines;
                 string[] intendedLines;
@@ -46,14 +90,14 @@ namespace TestRunner
                 int run;
                 for (run = 0; run < warmupRuns; run++)
                 {
-                    RunTest(fileNameIn, fileNameOut, testMethod, out outputLines, out intendedLines);
+                    RunTest(fileNameIn, fileNameOut, entrypoint, out outputLines, out intendedLines);
                 }
 
                 TimeSpan elapsed = TimeSpan.Zero;
                 run = 0;
                 do
                 {
-                    elapsed += RunTest(fileNameIn, fileNameOut, testMethod, out outputLines, out intendedLines);
+                    elapsed += RunTest(fileNameIn, fileNameOut, entrypoint, out outputLines, out intendedLines);
                 } while (++run < testRuns);
                 elapsed = new TimeSpan(elapsed.Ticks / testRuns);
 
@@ -90,7 +134,9 @@ namespace TestRunner
                 }
             }
 
-            if (failed == 0)
+            if(failed == 0 && passed == 0)
+                Console.WriteLine($"No tests found in {inputFolder}");
+            else if (failed == 0)
                 Console.WriteLine($"Passed all {totalElapsed.TotalMilliseconds:F5}ms");
             else if (passed == 0)
                 Console.WriteLine($"Failed all {totalElapsed.TotalMilliseconds:F5}ms");
@@ -130,7 +176,14 @@ namespace TestRunner
                 var stopwatch = new Stopwatch();
                 stopwatch.Reset();
                 stopwatch.Start();
-                testMethod();
+                try
+                {
+                    testMethod();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
                 stopwatch.Stop();
                 elapsed = stopwatch.Elapsed;
 
@@ -145,6 +198,25 @@ namespace TestRunner
                 intendedLines = Regex.Split(intended, "(\r\n|\r|\n)");
             }
             return elapsed;
+        }
+
+
+        static (Type returnType, Type[] parameterTypes, Func<MethodInfo, Action> adaptor)[] entryAdaptors = {
+            (typeof(void), new Type[0], mi => (Action)mi.CreateDelegate(typeof(Action))),
+            (typeof(void), new[] {typeof(string).MakeArrayType()}, mi => () => ((Action<string[]>) mi.CreateDelegate(typeof(Action<string[]>)))(Array.Empty<string>())),
+            (typeof(int), new Type[0], mi => () => ((Func<int>)mi.CreateDelegate(typeof(Func<int>)))()),
+            (typeof(int), new[] {typeof(string).MakeArrayType()}, mi => () => ((Func<string[], int>) mi.CreateDelegate(typeof(Func<string[], int>)))(Array.Empty<string>())),
+        };
+        private static Action BindMethodEntryPoint(MethodInfo methodInfo)
+        {
+
+            var parameterTypes = from param in methodInfo.GetParameters()
+                select param.ParameterType;
+            var methodInfos = from adaptor in entryAdaptors
+                where methodInfo.ReturnType == adaptor.returnType
+                where parameterTypes.SequenceEqual(adaptor.parameterTypes)
+                select adaptor.adaptor(methodInfo);
+            return methodInfos.FirstOrDefault();
         }
 
     }
